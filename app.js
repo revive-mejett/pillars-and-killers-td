b6de9c288ecb62e3a65b425ebfcd428cc9a20f0c
Kyle Veloso <kyle.veloso@dawsoncollege.qc.ca>
2023-04-09 19:07:17 -0400

    add picture for setup

--- app.js
+++ app.js
@@ -6,4 +6,7 @@ const app = new Application()
 document.body.appendChild(app.view)
 
 
+let chocolateHeliMeme = PIXI.Sprite.from('./assets/textures/chocolate_helicopter.jpg')
 
+app.stage.addChild(chocolateHeliMeme)
+console.log(chocolateHeliMeme)
