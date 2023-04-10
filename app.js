d9c3efccd380fe3d40141c276f2574892141396d
Kyle Veloso <kyle.veloso@dawsoncollege.qc.ca>
2023-04-09 22:18:07 -0400

    add rectangle

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 const Application = PIXI.Application
 
 const app = new Application()
@@ -7,12 +6,18 @@ document.body.appendChild(app.view)
 
 
 let chocolateHeliMeme = PIXI.Sprite.from('./assets/textures/chocolate_helicopter.jpg')
+let sprite = new PIXI.Sprite()
 
 app.stage.addChild(chocolateHeliMeme)
-console.log(chocolateHeliMeme.position)
 
-
-
+chocolateHeliMeme.height = 50
+chocolateHeliMeme.width = 50
+let graphics = new PIXI.Graphics()
+graphics.beginFill(0xFF0000)
+graphics.drawRect(200, 100, 100, 400)
+graphics.endFill()
+app.stage.addChild(graphics)
 app.ticker.add(() => {
     chocolateHeliMeme.x += 0.1
 })
+
