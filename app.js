73ef552f66101adb0b7f7ea58c933ba242a891cf
Kyle Veloso <kyle.veloso@dawsoncollege.qc.ca>
2023-04-09 22:54:23 -0400

    set up hard-coded grid

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const Application = PIXI.Application
+const mapSize = 1100
+const dimensions = 40
+const tileSize = mapSize / dimensions
 
-const app = new Application({width: 1000, height: 1000})
+const app = new Application({width: mapSize, height: mapSize})
 
 document.body.appendChild(app.view)
 
@@ -8,17 +11,21 @@ document.body.appendChild(app.view)
 let chocolateHeliMeme = PIXI.Sprite.from('./assets/textures/chocolate_helicopter.jpg')
 let sprite = new PIXI.Sprite()
 
-app.stage.addChild(chocolateHeliMeme)
+
 
 
 chocolateHeliMeme.height = 50
 chocolateHeliMeme.width = 50
+chocolateHeliMeme.zIndex = 3
 let graphics = new PIXI.Graphics()
 graphics.lineStyle(2, 0xAAAAAA)
-graphics.drawRect(200, 100, 100, 400)
-graphics.drawRect(200, 100, 100, 400)
-graphics.endFill()
+for (let i = 0; i < dimensions; i++) {
+    for (let j = 0; j < dimensions; j++) {
+        graphics.drawRect(i * tileSize, j * tileSize, tileSize, tileSize)
+    }
+}
 app.stage.addChild(graphics)
+app.stage.addChild(chocolateHeliMeme)
 let t = 0;
 app.ticker.add(() => {
     chocolateHeliMeme.x += 0.1
