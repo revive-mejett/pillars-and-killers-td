5736c238595fe9d3e89b496072ca54db0ef98a68
Kyle Veloso <kyle.veloso@dawsoncollege.qc.ca>
2024-08-31 16:00:22 -0400

    replace setinterval with ticker listner to spawn enemy per sec

--- app.js
+++ app.js
@@ -19,21 +19,18 @@ displayPath(app, myMap)
 const appTicker = new PIXI.Ticker();
 appTicker.autoStart = false
 
-appTicker.add((deltaTime) => {
-    walkPath(app, myMap)
-})
-
-let count = 0
-let waveInterval
-
-function spawnEnemy() {
-    walkPath(app, myMap)
-    count++
-    if (count >+ 10) {
-        clearInterval(waveInterval)
+let elapsedMS = 0
+function onTick() {
+    elapsedMS += appTicker.deltaMS
+    if (elapsedMS >= 1000) {
+        console.log("spawned");
+        elapsedMS = 0
+        
+        walkPath(app, myMap)
     }
 }
 
-waveInterval = setInterval(spawnEnemy, 700)
+appTicker.add(onTick)
+appTicker.start()
 
 
