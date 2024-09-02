4eb670740e9285e595e87d1198fbb13192348a3a
Kyle Veloso <kyle.veloso@dawsoncollege.qc.ca>
2024-09-02 00:10:58 -0400

    work on refactor enemy logic

--- src/objects/Enemy.js
+++ src/objects/Enemy.js
@@ -3,17 +3,32 @@ export class Enemy {
     /**
      *
      */
-    constructor(health, speed, textureFilePath, xPosition, yPosition) {
+    constructor(health, speed, xPosition, yPosition, textureFilePath) {
         super();
         this.health = health
         this.speed = speed
         this.textureFilePath = textureFilePath
-        this.position = { x: xPosition, x: yPosition}
+        this.position = { x: xPosition, y: yPosition }
+
+        this.xToNextWaypoint = 0
+        this.yToNextWaypoint = 0
+        this.nextWayPointIndex = 1
+        this.sprite = PIXI.Sprite.from('./assets/textures/chocolate_helicopter.jpg')
+
     }
 
     updatePos() {
 
     }
 
+    setDistancesToNext(map) {
+        
+        this.position.x = map.waypoints[this.nextWayPointIndex - 1].x * map.tileSize
+        this.position.y = map.waypoints[this.nextWayPointIndex - 1].y * map.tileSize
+        this.xToNextWaypoint = (map.waypoints[this.nextWayPointIndex].x * map.tileSize - this.position.x)
+        this.yToNextWaypoint = (map.waypoints[this.nextWayPointIndex].y * map.tileSize - this.position.y)
+        console.log(this.xToNextWaypoint)
+        console.log(this.yToNextWaypoint)
+    }
 
 }
