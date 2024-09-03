2a4a99b31a2c89be87a3caa84fc29527e2782e08
Kyle Veloso <kyle.veloso@dawsoncollege.qc.ca>
2024-09-02 23:57:41 -0400

    rmeove old walkpath method in map class

--- src/TdMap.js
+++ src/TdMap.js
@@ -113,55 +113,5 @@ function displayPath(app, map) {
 
 }
 
-function walkPath(app, map) {
-    let testEnemy = PIXI.Sprite.from('./assets/textures/chocolate_helicopter.jpg')
-    let waypoints = map.waypoints
-    testEnemy.height = map.tileSize
-    testEnemy.width = map.tileSize
-    testEnemy.zIndex = 3
-
-    let xToNextWaypoint = 0
-    let yToNextWaypoint = 0
-    let nextWayPointIndex = 1
-    let speed = 11
-    
-    let setDistancesToNext = () => {
-        
-        testEnemy.x = waypoints[nextWayPointIndex - 1].x * map.tileSize
-        testEnemy.y = waypoints[nextWayPointIndex - 1].y * map.tileSize
-        xToNextWaypoint = (waypoints[nextWayPointIndex].x * map.tileSize - testEnemy.x)
-        yToNextWaypoint = (waypoints[nextWayPointIndex].y * map.tileSize - testEnemy.y)
-        // console.log(xToNextWaypoint)
-        // console.log(yToNextWaypoint)
-    }
-
-
-    app.stage.addChild(testEnemy)
-    setDistancesToNext()
-    app.ticker.add(() => {
-
-        if (xToNextWaypoint !== 0) {
-            testEnemy.x += speed * (xToNextWaypoint > 0 ? 1 : -1) * app.ticker.deltaTime
-        }
-        else if (yToNextWaypoint !== 0) {
-            testEnemy.y += speed * (yToNextWaypoint > 0 ? 1 : -1) * app.ticker.deltaTime
-        }
-        xToNextWaypoint = (waypoints[nextWayPointIndex].x * map.tileSize - testEnemy.x)
-        yToNextWaypoint = (waypoints[nextWayPointIndex].y * map.tileSize - testEnemy.y)
-        if (Math.abs(xToNextWaypoint) < 1 && Math.abs(yToNextWaypoint) < 1 && nextWayPointIndex < waypoints.length) {
-            nextWayPointIndex++
-            if (nextWayPointIndex === waypoints.length) {
-                console.log("reached end")
-                testEnemy.destroy()
-            } else {
-                setDistancesToNext()
-            }
-            
-        }
-    })
-
-    
-    
-}
 
 export { TdMap, displayTiles, displayPath, walkPath }
