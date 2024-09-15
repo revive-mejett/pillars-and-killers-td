7612ec2e12aabab2a76890162e9d79656d7c67c1
Kyle Veloso <kyle.veloso@dawsoncollege.qc.ca>
2024-09-15 01:08:51 -0400

    remove load asset function for now

--- src/core/AssetLoader.js
+++ src/core/AssetLoader.js
@@ -13,18 +13,10 @@ export class AssetLoader {
         if (!instance) {
             instance = this
             this.resources = {}
-            this.loader = new PIXI.loader.shared
         }
 
 
         return instance
-    }
-
-    loadAssets() {
-        this.loader.add({
-            name : "greenEnemy",
-            url: "assets/images/killer_green_circle.png"
-        })
     }
     
 }
