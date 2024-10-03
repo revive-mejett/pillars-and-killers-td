c9a382f762cbe4c706d1eaa024c440cf246e151d
Kyle Veloso <kyle.veloso@dawsoncollege.qc.ca>
2024-10-03 15:14:04 -0400

    fix ui arrangement

--- src/UI/HUD.js
+++ src/UI/HUD.js
@@ -1,10 +1,12 @@
 import { Assets, Text, TextStyle } from "pixi.js"
 
 export class HUD {
+
+
     constructor() {
         this.container = new PIXI.Container()
-        this.container.width = 200
-        this.container.height = 200
+        this.container.width = 1100 * 0.25
+        this.container.height = 1000
         this.container.x = 1100
         this.container.y = 0
     }
@@ -14,7 +16,7 @@ export class HUD {
 
         const bgColor = new PIXI.Graphics()
         bgColor.beginFill(0x002222)
-        bgColor.drawRect(0,0,1100*0.2,1100)
+        bgColor.drawRect(0,0,1100 * 0.25,1100)
         bgColor.endFill()
         container.zIndex = 99999
         this.container.zIndex = 99999
@@ -22,36 +24,64 @@ export class HUD {
         this.container.addChild(bgColor)
 
         let iconBundle = await Assets.loadBundle("icons")
-        console.log(iconBundle)
+
+
+        //add money ui
+        const moneyContainer = new PIXI.Container()
+        moneyContainer.x = 0
+        moneyContainer.y = 0
+        moneyContainer.width = 200
+        moneyContainer.height = 100
+        this.container.addChild(moneyContainer)
+
+        const moneyContainerbg = new PIXI.Graphics()
+        moneyContainer.addChild(moneyContainerbg)
+        moneyContainerbg.beginFill(0x003300)
+        moneyContainerbg.drawRect(0,0, this.container.width, 100)
+        moneyContainerbg.endFill()
+        
 
         const moneySprite = PIXI.Sprite.from(iconBundle.money)
         moneySprite.height = 100
         moneySprite.width = 100
         moneySprite.x = 0
-        moneySprite.y = 100
-        this.container.addChild(moneySprite)
+        moneySprite.y = 0
+        moneyContainer.addChild(moneySprite)
 
 
-        const moneyText = new Text("300", new TextStyle({fontFamily: "Times New Roman", fontSize: 40, fill: 0xFFFF00}))
-        moneyText.x = 100
-        moneyText.y = 0
-        this.container.addChild(moneyText)
+        const moneyText = new Text("1337", new TextStyle({fontFamily: "Times New Roman", fontSize: 40, fill: 0xFFFF00, align: 'center'}))
+        moneyText.x = 0 + moneySprite.width
+        moneyText.y = (moneyContainer.height - moneyText.height) / 2;
+        moneyContainer.addChild(moneyText)
 
+        //add lives ui
+        const livesContainer = new PIXI.Container()
+        livesContainer.x = 0
+        livesContainer.y = 100
+        livesContainer.width = 200
+        livesContainer.height = 100
+        this.container.addChild(livesContainer)
+
+        const livesContainerbg = new PIXI.Graphics()
+        livesContainer.addChild(livesContainerbg)
+        livesContainerbg.beginFill(0x330000)
+        livesContainerbg.drawRect(0,0, this.container.width, 100)
+        livesContainerbg.endFill()
 
 
         const livesSprite = PIXI.Sprite.from(iconBundle.lives)
         livesSprite.height = 100
         livesSprite.width = 100
         livesSprite.x = 0
-        livesSprite.y = 100
-        this.container.addChild(livesSprite)
+        livesSprite.y = 0
+        livesContainer.addChild(livesSprite)
 
         const livesText = new Text("300", new TextStyle({fontFamily: "Times New Roman", fontSize: 40, fill: 0xFF00, align: 'center'}))
-        livesText.x = 100
-        livesText.y = 100
-        this.container.addChild(livesText)
+        livesText.x = 0 + livesSprite.width
+        livesText.y = (livesContainer.height - livesText.height) / 2;
+        livesContainer.addChild(livesText)
 
-        // const livesIcon = new PIXI.Spr
+        
 
     }
 }
