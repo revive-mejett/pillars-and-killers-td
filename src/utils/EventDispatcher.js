acda25881b7888d8411c6f6e131ee50ecf25fdd8
Kyle Veloso <kyle.veloso@dawsoncollege.qc.ca>
2024-10-07 00:31:03 -0400

    add EventDispatcher.js to manage events

--- src/utils/EventDispatcher.js
+++ src/utils/EventDispatcher.js
@@ -7,7 +7,38 @@ export class EventDispatcher {
         //singleton
         if (!instance) {
             instance = this
+            this.listeners = {}
         }
         return instance
     }
+
+    //register an event handler
+    on(event, callback) {
+        //register handler
+        if (!this.listeners[event]) {
+            this.listeners[event] = []
+        }
+        //add the event handler function to list of handlers
+        this.listeners[event].push(callback)
+    }
+
+    //remove an event handler function from the event
+    off(event, callbackToRemove) {
+        //if no such event is register
+        if (!this.listeners[event]) {
+            return
+        }
+        //add the event handler function to list of handlers
+        this.listeners[event].filter(callback => callback != callbackToRemove)
+    }
+
+    fireEvent(event, data) {
+        if (!this.listeners[event]) {
+            console.warn("no such event is registered -- ", event)
+            return
+        }
+        //call all event handlers for that event
+        this.listeners.event.forEach(callback => callback())
+    }
+
 }
