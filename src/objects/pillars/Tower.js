e79b4f3a2f51e4c9c4fecdc0936da121ddf7cb40
Kyle Veloso <kyle.veloso@dawsoncollege.qc.ca>
2024-10-12 12:36:07 -0400

    Add tower class

---
+++ src/objects/pillars/Tower.js
@@ -0,0 +1,14 @@
+
+//base class for tower
+export class Tower {
+    constructor(range, damage, fireRate, cost, sprite) {
+        this.range = range
+        this.damage = damage
+        this.fireRate = fireRate
+    }
+
+    //todo not implemented
+    fire() {
+        
+    }
+}
