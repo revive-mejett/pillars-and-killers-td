a15a762799fcd5c53b4c28bd8edc54a4e60f1ace
Kyle Veloso <kyle.veloso@dawsoncollege.qc.ca>
2024-10-12 21:37:56 -0400

    add mark tower type method

--- src/objects/Tile.js
+++ src/objects/Tile.js
@@ -3,12 +3,19 @@ import { Entity } from "./Entity";
 export class Tile extends Entity {
 
     /**
-     * tileType can be either "startTile", "endTile", "pathTile", "grassTile", "towerTile"
+     * tileType can be either "startTile", "endTile", "pathTile", "grassTile"
      */
-    constructor(x, y, width, height, tileType) {
+    constructor(x, y, width, height, tileType, graphics) {
         super(x, y, width, height);
+        this.tileType = tileType
+        this.graphics = graphics
     }
 
-    
+    markTowerOccupied(hasTower) {
+        if (this.tileType != "grassTile") {
+            throw new Error("Must be a grass tile")
+        }
+        this.hasTower = hasTower
+    }
 
 }
