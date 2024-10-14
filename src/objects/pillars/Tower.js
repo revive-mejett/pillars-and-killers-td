962d90af7e9263e449da4069b000d3995bff2899
Kyle Veloso <kyle.veloso@dawsoncollege.qc.ca>
2024-10-14 12:55:38 -0400

    Add Tower.js

--- src/objects/pillars/Tower.js
+++ src/objects/pillars/Tower.js
@@ -1,10 +1,14 @@
+import { Entity } from "../Entity.js"
 
 //base class for tower
-export class Tower {
-    constructor(range, damage, fireRate, cost, sprite) {
+export class Tower extends Entity {
+    constructor(x, y, width, height, range, damage, fireRate, cost, asset) {
+        super(x, y, width, height)
         this.range = range
         this.damage = damage
-        this.fireRate = fireRate
+        this.fireRate = fireRate,
+        this.cost = cost,
+        this.asset = asset
     }
 
     //todo not implemented
