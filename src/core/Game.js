67c0a7a8d6d28b0d725f836112c06322e4d529ef
Kyle Veloso <kyle.veloso@dawsoncollege.qc.ca>
2024-10-22 11:39:59 -0400

    move properties of gamestate and assetloader

--- src/core/Game.js
+++ src/core/Game.js
@@ -3,14 +3,14 @@ import { GameplayScene } from "../scenes/GameplayScene.js"
 import { AssetLoader } from "./AssetLoader.js"
 import { GameState } from "./GameState.js"
 
+const assetLoader = new AssetLoader()
+
 export class Game {
     constructor() {
         this.mapSize = 1000
         this.dimensions = 25
         this.app = new PIXI.Application({width: this.mapSize + 250, height: this.mapSize})
-        this.gameState = new GameState()
         this.gameplayScene = undefined
-        this.assetLoader = new AssetLoader()
     }
 
     start() {
@@ -18,11 +18,11 @@ export class Game {
     }
 
     async setup() {
-        await this.assetLoader.bundleAssets()
-        await this.assetLoader.loadEnemySprites()
-        await this.assetLoader.loadIconSprites()
-        await this.assetLoader.loadTowerSprites()
-        this.gameplayScene = new GameplayScene(this.gameState, this.app)
+        assetLoader.bundleAssets()
+        await assetLoader.loadEnemySprites()
+        await assetLoader.loadIconSprites()
+        await assetLoader.loadTowerSprites()
+        this.gameplayScene = new GameplayScene(this.app)
         this.gameplayScene.buildMap()
     }
 
