df4abb16a65e2e5e8297d08995a97206c12bb882
Kyle Veloso <kyle.veloso@dawsoncollege.qc.ca>
2024-11-30 10:27:29 -0500

    revert wave

--- src/managers/WaveManager.ts
+++ src/managers/WaveManager.ts
@@ -37,7 +37,7 @@ export class WaveManager {
         this.waves = []
         this.extraWaves = this.generateExtraWaves()
 
-        this.currentWave = 110
+        this.currentWave = 0
         this.waveInProgress = false
 
         this.cooldownToNextWave = 0
