ea847810cb2ffc9dcbe480cc8713c708a11f2438
Kyle Veloso <kyle.veloso@dawsoncollege.qc.ca>
2024-12-01 21:09:32 -0500

    playtest 20 waves again

--- src/core/GameState.ts
+++ src/core/GameState.ts
@@ -9,7 +9,7 @@ export class GameState {
 
     constructor() {
         this.lives = 50
-        this.money = 300
+        this.money = 10000
         this.uiManager = undefined
 
         eventDispatcher.on("enemyReachEnd", this.loseLives.bind(this))
