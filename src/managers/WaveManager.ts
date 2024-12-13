28ee34e4e7cd246452131eb3f981cda12e8cfcaa
Kyle Veloso <kyle.veloso@dawsoncollege.qc.ca>
2024-12-12 23:53:04 -0500

    set freeplay true after ton 618 killed

--- src/managers/WaveManager.ts
+++ src/managers/WaveManager.ts
@@ -71,7 +71,11 @@ export class WaveManager {
         eventDispatcher.on("boss2Killed", () => this.updateNextCheckpointWave(2))
         eventDispatcher.on("boss3Killed", () => this.updateNextCheckpointWave(3))
         eventDispatcher.on("boss4Killed", () => this.updateNextCheckpointWave(4))
-        eventDispatcher.on("boss5Killed", () => console.log("WINNER!!! TODO implement winner ui and option freeplay!"))
+        eventDispatcher.on("boss5Killed", () => {
+            console.log("WINNER!!! TODO implement winner ui and option freeplay!")
+            this.bossPresent = false
+            this.cooldownToNextWave = 0
+        })
 
     }
 
