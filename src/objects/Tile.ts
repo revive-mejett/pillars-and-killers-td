ba42740b5e6074276e0d29ceae567077ad093d89
Kyle Veloso <kyle.veloso@dawsoncollege.qc.ca>
2024-12-22 23:05:36 -0500

    minute changes

--- src/objects/Tile.ts
+++ src/objects/Tile.ts
@@ -122,7 +122,7 @@ export class Tile extends Entity {
 
             this.container?.removeChildren()
             const tileBackground = new PIXI.Graphics()
-            tileBackground.lineStyle(2, this.grassOutlineColour)
+            tileBackground.lineStyle(2, this.tower.tileColour)
             tileBackground.beginFill(this.tower.tileColour)
             tileBackground.drawRect(this.x, this.y, this.width, this.height)
             tileBackground.endFill()
