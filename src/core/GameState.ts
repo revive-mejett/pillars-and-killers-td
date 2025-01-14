c15cdb76a441ed03d35e50871d7c0e1d8cb1603d
Kyle Veloso <kyle.veloso@dawsoncollege.qc.ca>
2025-01-14 01:43:47 -0500

    revert game stats

--- src/core/GameState.ts
+++ src/core/GameState.ts
@@ -11,9 +11,9 @@ const developerOffSet = 0
 
 export class GameState {
     lives: number = 100
-    money: number = 400000
+    money: number = 400
     uiManager?: UIManager
-    startWave: number = 99
+    startWave: number = 0
     mapName: string = "Walk in the Park"
     saveFileIndex: 1 | 2 | 3 | 4 | 5 | 6 = 1
     researchLevel: 1 | 2 | 3 | 4 = 1
