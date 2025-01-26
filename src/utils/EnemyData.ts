97f9e03fd72d93be94e71f1c996e1a551e3ad650
Kyle Veloso <kyle.veloso@dawsoncollege.qc.ca>
2025-01-26 15:28:12 -0500

    minute change

--- src/utils/EnemyData.ts
+++ src/utils/EnemyData.ts
@@ -2164,7 +2164,7 @@ const allEnemyData: EnemyData = {
             className: "TON 618",
             type: "Boss",
             health: 175_571_777,
-            speed: 1,
+            speed: 0.25,
             damage: 99999,
             killValue: 1000000,
             rotationSpeed: 0.01,
